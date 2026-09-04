1a67141a027a7e5daadbee6479487d68ff5d4ecc
webcreation111 <kovilangulam1@gmail.com>
2026-09-05 01:58:42 +0530

    Update config.php

--- config.php
+++ config.php
@@ -9,4 +9,5 @@ $mysquli=mysqli_connect($hostname,$username,$password,$database);
 if (!$mysquli) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+    echo "Database connected successfully";
 ?>
